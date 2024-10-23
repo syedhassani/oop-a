1f6034def30d00ee89253d29215e6bf6ebf7c9bc
syedhassani <syedmhassani12@gmail.com>
2024-10-23 20:15:38 +1100

    opps1

---
+++ main.py
@@ -0,0 +1,16 @@
+# class student:
+#     name="jim"
+#     grade=20
+#     print("hello i am ",name,"i am in grade",grade)
+
+# obj=student()
+# activtiy 2
+class student:
+   name="ali"
+   grade= 6
+   def intro(self):
+      print("hello i am", self.name,"i am in grade",self.grade)
+obj=student()
+obj.intro()
+ 
+
